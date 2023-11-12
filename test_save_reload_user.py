5e178bc2a1c69169a37ba16beabf2b0625a42a14
el-hatem <elhaatem@gmail.com>
2023-11-12 22:02:22 +0200

    Ahmed

--- test_save_reload_user.py
+++ test_save_reload_user.py
@@ -18,7 +18,7 @@ my_user.password = "root"
 my_user.save()
 print(my_user)
 
-print("-- Create a new User 2 --")
+print("------ Create a new User 2 -----")
 my_user2 = User()
 my_user2.first_name = "John"
 my_user2.email = "airbnb2@mail.com"
