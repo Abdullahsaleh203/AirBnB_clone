654f64488be476aceb455e3304570a914605b721
el-hatem <elhaatem@gmail.com>
2023-11-12 21:56:00 +0200

    Ahmed

--- test_save_reload_base_model.py
+++ test_save_reload_base_model.py
@@ -8,7 +8,8 @@ for obj_id in all_objs.keys():
     obj = all_objs[obj_id]
     print(obj)
 
-print("-- Create a new object --")
+
+print("------ Create a new object ----")
 my_model = BaseModel()
 my_model.name = "My_First_Model"
 my_model.my_number = 89
